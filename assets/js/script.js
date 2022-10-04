bafbb4b3d5ed634dc5ab1bc5ed3d0b1fc1ed83cb
junedkaxioned <junedk@axioned.com>
2022-10-04 16:35:00 +0530

    JS Complted

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,8 @@
-/* Author: Zishan Ansari */
+var toggleBtn = document.querySelectorAll('.faq-btn');
+
+// function for Adding Event on each btn
+toggleBtn.forEach(function (toggel) {
+  toggel.addEventListener('click', function () {
+    toggel.parentNode.classList.toggle('active');
+  })
+})
